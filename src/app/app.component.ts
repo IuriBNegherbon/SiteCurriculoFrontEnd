09117a0a5de40128473f8e73ec046f4a469a1483
Iuri Bruning Negherbon <iuri.bruning@gmail.com>
2024-08-01 23:48:50 -0300

    Ajuste conflitos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { SocialAuthService } from '@abacritt/angularx-social-login';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,16 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Site Curriculo Iuri Bruning Negherbon';
-  user:any;
-  loggedIn:any;
-
-  constructor(private authService: SocialAuthService) { }
-
-  ngOnInit() {
-    this.authService.authState.subscribe((user) => {
-      this.user = user;
-      this.loggedIn = (user != null);
-      console.log(this.user);
-    });
-  }
 }
